ead73f398d0b31dedf5aca50209f2aee38bef4fc
Juho Ervasti <juho.ervasti@gispo.fi>
2025-09-01 16:16:42 +0300

    scripts: save positional arguments before shifting them out of existence

--- scripts/split_project_and_upload.sh
+++ scripts/split_project_and_upload.sh
@@ -7,7 +7,7 @@ repo_dir=$(readlink -f "$script_dir/..")
 python_cli=$(readlink -f "$repo_dir/src/pm.py")
 
 function _show_usage() {
-  echo "Usage: $0 TEMPLATE_PROJECT_DIRECTORY OUTPUT_DIRECTORY CONFIG_GPKG WORKSPACE PROJECT_NAME_PREFIX"
+  echo "Usage: $0 TEMPLATE_PROJECT_DIRECTORY FULL_DATA_DIRECTORY OUTPUT_DIRECTORY CONFIG_GPKG WORKSPACE PROJECT_NAME_PREFIX"
   echo -e "Splits a template project to parts and uploads them to a Mergin Maps server"
   echo -e "Arguments:"
   echo -e "\t TEMPLATE_PROJECT_DIRECTORY"
@@ -30,6 +30,17 @@ function _check_argument() {
   fi
 }
 
+# you could check that these exist, but the Python CLI does that anyway, so probably no point
+template_project_dir=$1
+full_data_dir=$2
+output_dir=$3
+config_gpkg=$4
+workspace=$5
+project_name_prefix=$6
+
+# NOTE: this ordering does not really work, or at least wouldn't if there were
+# any other accepted options, so if you add one, take that into account
+
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -h|--help)
@@ -43,14 +54,6 @@ while [[ "$#" -gt 0 ]]; do
     esac
     shift
 done
-
-# you could check that these exist, but the Python CLI does that anyway, so probably no point
-template_project_dir=$1
-full_data_dir=$2
-output_dir=$3
-config_gpkg=$4
-workspace=$5
-project_name_prefix=$6
 
 _check_argument "TEMPLATE_PROJECT_DIRECTORY" "$template_project_dir"
 _check_argument "FULL_DATA_DIRECTORY" "$full_data_dir"
